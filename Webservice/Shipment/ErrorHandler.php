af7ce6f3f03ee2aee921fb74e016b1b9bd97a250
Rico Sonntag <rico.sonntag@netresearch.de>
2018-12-17 13:50:41 +0100

    DHLGW-178: Added error handler

---
+++ Webservice/Shipment/ErrorHandler.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+declare(strict_types=1);
+
+namespace Dhl\Paket\Webservice\Shipment;
+
+use Dhl\Sdk\Bcs\Webservice\Exception\ValidationStatusException;
+use Dhl\Sdk\Bcs\Webservice\Exception\WeakValidationException;
+use Magento\Framework\DataObject;
+use Magento\Framework\DataObjectFactory;
+
+/**
+ * ErrorHandler class.
+ *
+ * @author  Rico Sonntag <rico.sonntag@netresearch.de>
+ * @license https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link    https://www.netresearch.de/
+ */
+class ErrorHandler
+{
+    /**
+     * @var DataObjectFactory
+     */
+    private $dataObjectFactory;
+
+    /**
+     * Constructor.
+     *
+     * @param DataObjectFactory $dataObjectFactory
+     */
+    public function __construct(
+        DataObjectFactory $dataObjectFactory
+    ) {
+        $this->dataObjectFactory = $dataObjectFactory;
+    }
+
+    /**
+     * @param \Exception $ex
+     *
+     * @return DataObject
+     */
+    public function createErrorResult(\Exception $ex): DataObject
+    {
+        if ($ex instanceof ValidationStatusException) {
+            $messages = array_unique($ex->getStatus()->getStatusMessage());
+            $message  = 'Failed to create shipment label: ' . implode(', ', $messages);
+        } else {
+            $message = $ex->getMessage();
+        }
+
+        return $this->dataObjectFactory->create([
+            'data' => [
+                'errors' => $message,
+            ],
+        ]);
+    }
+}
