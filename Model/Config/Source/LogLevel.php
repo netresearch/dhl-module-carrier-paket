a7b51a36b74fa6824e82ddcb0a31a66ea5ae96f0
Sebastian Ertner <sebastian.ertner@netresearch.de>
2019-02-22 17:09:17 +0100

    DHLGW-210: add api logger

---
+++ Model/Config/Source/LogLevel.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+declare(strict_types=1);
+
+namespace Dhl\Paket\Model\Config\Source;
+
+use Magento\Framework\Logger\Monolog;
+use Magento\Framework\Option\ArrayInterface;
+
+/**
+ * Class LogLevel
+ *
+ * @author    Ronny Gertler <ronny.gertler@netresearch.de>
+ * @copyright 2018 Netresearch GmbH & Co. KG
+ * @link      http://www.netresearch.de/
+ */
+class LogLevel implements ArrayInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray(): array
+    {
+        return [
+            ['value' => (string) Monolog::ERROR, 'label' => __('Errors')],
+            ['value' => (string) Monolog::WARNING,'label' => __('Warnings')],
+            ['value' => (string) Monolog::INFO, 'label' => __('Info (All API Activities)')],
+        ];
+    }
+}
