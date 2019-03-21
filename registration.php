1b6032d149e9ffc304d93fd888a730f28629f68f
Rico Sonntag <rico.sonntag@netresearch.de>
2019-03-21 09:09:23 +0100

    DHLGW-268: Add license reference in file header

--- registration.php
+++ registration.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * See LICENSE.md for license details.
+ */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Dhl_Paket',
