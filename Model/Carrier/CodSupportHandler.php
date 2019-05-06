5c18e1855d3875e2f747cf43028a572b659ea265
Rico Sonntag <rico.sonntag@netresearch.de>
2019-05-06 12:51:56 +0200

    DHLGW-301: Handle "preferredLocation" and "preferredNeighbour" checkout services

--- Model/Carrier/CodSupportHandler.php
+++ Model/Carrier/CodSupportHandler.php
@@ -7,7 +7,11 @@ declare(strict_types=1);
 namespace Dhl\Paket\Model\Carrier;
 
 use Dhl\ShippingCore\Api\CodSupportInterface;
+use Dhl\ShippingCore\Api\Data\ShippingOption\Selection\AssignedSelectionInterface;
 use Dhl\ShippingCore\Model\Config\CoreConfig;
+use Dhl\ShippingCore\Model\ShippingOption\Selection\QuoteSelectionRepository;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\SearchCriteriaBuilderFactory;
 use Magento\Quote\Model\Quote;
 
 /**
@@ -25,13 +29,38 @@ class CodSupportHandler implements CodSupportInterface
     private $config;
 
     /**
+     * @var QuoteSelectionRepository
+     */
+    private $quoteSelectionRepository;
+
+    /**
+     * @var FilterBuilder
+     */
+    private $filterBuilder;
+
+    /**
+     * @var SearchCriteriaBuilderFactory
+     */
+    private $searchCriteriaBuilderFactory;
+
+    /**
      * CodSupportHandler constructor.
      *
+     * @param SearchCriteriaBuilderFactory $searchCriteriaBuilderFactory
+     * @param FilterBuilder $filterBuilder
+     * @param QuoteSelectionRepository $quoteSelectionRepository
      * @param CoreConfig $config
      */
-    public function __construct(CoreConfig $config)
-    {
-        $this->config = $config;
+    public function __construct(
+        SearchCriteriaBuilderFactory $searchCriteriaBuilderFactory,
+        FilterBuilder $filterBuilder,
+        QuoteSelectionRepository $quoteSelectionRepository,
+        CoreConfig $config
+    ) {
+        $this->searchCriteriaBuilderFactory = $searchCriteriaBuilderFactory;
+        $this->filterBuilder                = $filterBuilder;
+        $this->quoteSelectionRepository     = $quoteSelectionRepository;
+        $this->config                       = $config;
     }
 
     /**
@@ -44,17 +73,59 @@ class CodSupportHandler implements CodSupportInterface
      * Note: No need to validate origin country. Paket carrier is only available for DE or AT origin checkouts anyway.
      *
      * @param Quote $quote
+     *
      * @return bool
      */
     public function hasCodSupport(Quote $quote): bool
     {
+        return $this->isDomesticShipment($quote)
+            && !$this->hasCodIncompatibleServices($quote);
+    }
+
+    /**
+     * Returns TRUE if the shipment is a domestic shipment (DE-DE or AT-AT).
+     *
+     * @param Quote $quote
+     *
+     * @return bool
+     */
+    private function isDomesticShipment(Quote $quote): bool
+    {
         $originCountryId = $this->config->getOriginCountry($quote->getStoreId());
-        $destCountryId = $quote->getShippingAddress()->getCountryId();
-        $isDomestic = ($originCountryId === $destCountryId);
+        $destCountryId   = $quote->getShippingAddress()->getCountryId();
 
-        //todo(nr): Handle preferredLocation and preferredNeighbour when selectable in checkout
-        $hasCodIncompatibleServices = false;
+        return $originCountryId === $destCountryId;
+    }
 
-        return $isDomestic && !$hasCodIncompatibleServices;
+    /**
+     * Returns TRUE if a COD incompatible service is used.
+     *
+     * @param Quote $quote
+     *
+     * @return bool
+     */
+    private function hasCodIncompatibleServices(Quote $quote): bool
+    {
+        $parentIdFilter = $this->filterBuilder
+            ->setField(AssignedSelectionInterface::PARENT_ID)
+            ->setConditionType('eq')
+            ->setValue($quote->getShippingAddress()->getId())
+            ->create();
+
+        $optionCodeFilter = $this->filterBuilder
+            ->setField(AssignedSelectionInterface::SHIPPING_OPTION_CODE)
+            ->setConditionType('in')
+            ->setValue(implode(',', ['preferredLocation', 'preferredNeighbour']))
+            ->create();
+
+        $searchCriteria = $this->searchCriteriaBuilderFactory
+            ->create()
+            ->addFilter($parentIdFilter)
+            ->addFilter($optionCodeFilter)
+            ->create();
+
+        return (bool) $this->quoteSelectionRepository
+            ->getList($searchCriteria)
+            ->count();
     }
 }
