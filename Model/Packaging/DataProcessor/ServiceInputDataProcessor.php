4bbf3fefd1c216c7ffdbad1d59c5583abe869bbd
Christoph Aßmann <christoph.assmann@netresearch.de>
2019-07-17 22:24:46 +0200

    DHLGW-445: fix preferred day/time api values

--- Model/Packaging/DataProcessor/ServiceInputDataProcessor.php
+++ Model/Packaging/DataProcessor/ServiceInputDataProcessor.php
@@ -10,6 +10,7 @@ use Dhl\Paket\Model\Carrier\Paket;
 use Dhl\ShippingCore\Api\Data\ShippingOption\ShippingOptionInterface;
 use Dhl\ShippingCore\Model\Packaging\AbstractProcessor;
 use Dhl\ShippingCore\Model\Packaging\PackagingDataProvider;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 use Magento\Sales\Model\Order\Shipment;
 
 /**
@@ -20,6 +21,80 @@ use Magento\Sales\Model\Order\Shipment;
  */
 class ServiceInputDataProcessor extends AbstractProcessor
 {
+    /**
+     * @var TimezoneInterface
+     */
+    private $timezone;
+
+    /**
+     * ServiceInputDataProcessor constructor.
+     * @param TimezoneInterface $timezone
+     */
+    public function __construct(TimezoneInterface $timezone)
+    {
+        $this->timezone = $timezone;
+    }
+
+    /**
+     * Infer radio button label from selection value.
+     *
+     * @param ShippingOptionInterface $shippingOption
+     */
+    private function processParcelStationInputs(ShippingOptionInterface $shippingOption)
+    {
+        foreach ($shippingOption->getInputs() as $input) {
+            if ($input->getCode() === 'id') {
+                list($stationId, $countryId, $postalCode, $city) = explode('|', $input->getDefaultValue());
+                $input->setLabel(
+                    sprintf(
+                        'Packstation %s, %s %s %s',
+                        $stationId,
+                        $countryId,
+                        $postalCode,
+                        $city
+                    )
+                );
+            }
+        }
+    }
+
+    /**
+     * Infer radio button label from selection value.
+     *
+     * @param ShippingOptionInterface $shippingOption
+     */
+    private function processPreferredDayInputs(ShippingOptionInterface $shippingOption)
+    {
+        foreach ($shippingOption->getInputs() as $input) {
+            if ($input->getCode() === 'date') {
+                $dateFormatted = $this->timezone->formatDate(
+                    $input->getDefaultValue(),
+                    \IntlDateFormatter::MEDIUM
+                );
+
+                $input->setLabel($dateFormatted);
+            }
+        }
+    }
+
+    /**
+     * Infer radio button label from selection value.
+     *
+     * @param ShippingOptionInterface $shippingOption
+     */
+    private function processPreferredTimeInputs(ShippingOptionInterface $shippingOption)
+    {
+        foreach ($shippingOption->getInputs() as $input) {
+            if ($input->getCode() === 'time') {
+                $timeRange = str_split($input->getDefaultValue(), 4);
+                $startTime = implode(':', str_split($timeRange[0], 2));
+                $endTime = implode(':', str_split($timeRange[1], 2));
+
+                $input->setLabel($startTime . ' - ' . $endTime);
+            }
+        }
+    }
+
     /**
      * @param ShippingOptionInterface[] $optionsData
      * @param Shipment $shipment
@@ -38,25 +113,17 @@ class ServiceInputDataProcessor extends AbstractProcessor
             return $optionsData;
         }
 
-        foreach ($optionsData as $shippingOption) {
-            if ($shippingOption->getCode() !== 'parcelstation') {
-                // not interested in anything else but parcel station service
-                continue;
-            }
-
-            foreach ($shippingOption->getInputs() as $input) {
-                if ($input->getCode() === 'id') {
-                    list($stationId, $countryId, $postalCode, $city) = explode('|', $input->getDefaultValue());
-                    $input->setLabel(
-                        sprintf(
-                            'Packstation %s, %s %s %s',
-                            $stationId,
-                            $countryId,
-                            $postalCode,
-                            $city
-                        )
-                    );
-                }
+        foreach ($optionsData as $optionGroup) {
+            switch ($optionGroup->getCode()) {
+                case 'parcelstation':
+                    $this->processParcelStationInputs($optionGroup);
+                    break;
+                case 'preferredDay':
+                    $this->processPreferredDayInputs($optionGroup);
+                    break;
+                case 'preferredTime':
+                    $this->processPreferredTimeInputs($optionGroup);
+                    break;
             }
         }
 
