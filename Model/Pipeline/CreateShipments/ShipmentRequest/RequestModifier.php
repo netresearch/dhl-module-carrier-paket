d043c4a627245c76d76779315609cb31db415e72
Sebastian Ertner <sebastian.ertner@netresearch.de>
2020-07-13 15:52:41 +0200

    DHLGW-943: read shipping product from package options during bulk operations

--- Model/Pipeline/CreateShipments/ShipmentRequest/RequestModifier.php
+++ Model/Pipeline/CreateShipments/ShipmentRequest/RequestModifier.php
@@ -6,7 +6,6 @@ declare(strict_types=1);
 
 namespace Dhl\Paket\Model\Pipeline\CreateShipments\ShipmentRequest;
 
-use Dhl\Paket\Util\ShippingProducts;
 use Dhl\ShippingCore\Api\ConfigInterface;
 use Dhl\ShippingCore\Api\Pipeline\ShipmentRequest\RequestModifier\PackagingOptionReaderInterfaceFactory;
 use Dhl\ShippingCore\Api\Pipeline\ShipmentRequest\RequestModifierInterface;
@@ -31,11 +30,6 @@ class RequestModifier implements RequestModifierInterface
     private $config;
 
     /**
-     * @var ShippingProducts
-     */
-    private $shippingProducts;
-
-    /**
      * @var PackagingOptionReaderInterfaceFactory
      */
     private $packagingOptionReaderFactory;
@@ -50,73 +44,19 @@ class RequestModifier implements RequestModifierInterface
      *
      * @param RequestModifierInterface $coreModifier
      * @param ConfigInterface $config
-     * @param ShippingProducts $shippingProducts
      * @param PackagingOptionReaderInterfaceFactory $packagingOptionReaderFactory
      * @param DataObjectFactory $dataObjectFactory
      */
     public function __construct(
         RequestModifierInterface $coreModifier,
         ConfigInterface $config,
-        ShippingProducts $shippingProducts,
         PackagingOptionReaderInterfaceFactory $packagingOptionReaderFactory,
         DataObjectFactory $dataObjectFactory
     ) {
         $this->coreModifier = $coreModifier;
         $this->config = $config;
-        $this->shippingProducts = $shippingProducts;
         $this->packagingOptionReaderFactory = $packagingOptionReaderFactory;
         $this->dataObjectFactory = $dataObjectFactory;
-    }
-
-    /**
-     * Add default shipping product to package params, e.g. V01PAK or V53PAK
-     *
-     * @param Request $shipmentRequest
-     * @throws LocalizedException
-     */
-    private function modifyPackage(Request $shipmentRequest)
-    {
-        $originCountry = $shipmentRequest->getShipperAddressCountryCode();
-        $destinationCountry = $shipmentRequest->getRecipientAddressCountryCode();
-
-        // load applicable products for the current route
-        $euCountries = $this->config->getEuCountries($shipmentRequest->getOrderShipment()->getStoreId());
-        $applicableProducts = $this->shippingProducts->getShippingProducts(
-            $originCountry,
-            $destinationCountry,
-            $euCountries
-        );
-
-        // check if defaults applicable to the current route are configured
-        $defaults = array_intersect_key(
-            $this->shippingProducts->getDefaultProducts($originCountry),
-            $applicableProducts
-        );
-
-        $defaultProduct = current($defaults);
-        $applicableProductCodes = current($applicableProducts);
-        if (!in_array($defaultProduct, $applicableProductCodes, true)) {
-            $message = __(
-                'The product %1 is not valid for the route %2-%3.',
-                $defaultProduct,
-                $originCountry,
-                $destinationCountry
-            );
-            throw new LocalizedException($message);
-        }
-
-        $packages = [];
-        foreach ($shipmentRequest->getData('packages') as $packageId => $package) {
-            $package['params']['shipping_product'] = $defaultProduct;
-            $packages[$packageId] = $package;
-        }
-
-        // set all updated packages to request
-        $shipmentRequest->setData('packages', $packages);
-
-        // add current package's params to request (compare AbstractCarrierOnline::requestToShipment)
-        $package = $packages[$shipmentRequest->getData('package_id')];
-        $shipmentRequest->setData('package_params', $this->dataObjectFactory->create(['data' => $package['params']]));
     }
 
     /**
@@ -170,9 +110,6 @@ class RequestModifier implements RequestModifierInterface
     {
         // add carrier-agnostic data
         $this->coreModifier->modify($shipmentRequest);
-
-        // add carrier-specific data
-        $this->modifyPackage($shipmentRequest);
         $this->modifyCustoms($shipmentRequest);
     }
 }
